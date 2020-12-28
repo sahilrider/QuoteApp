4096d132dad3bf948647d292221d3b82339c524b
sahilrider <sahilriders@gmail.com>
2020-12-28 23:57:39 +0530

    bugs fix

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true})
         })
 
         //Listening
-        app.listen(3000, function() {
+        app.listen(process.env.PORT || 3000, function() {
             console.log('listening on 3000')
         })
     })
